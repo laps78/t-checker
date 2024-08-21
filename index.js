fe0e4db7556909048586869145547e01b37b8fbe
laps78 <sovtem@gmail.com>
2024-08-21 12:14:41 +0300

    Подключены мидлвары logger, saveToFS

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
-const env = require('dotenv').config()
+
+const dotenv = require('dotenv')
+dotenv.config()
+
 const app = express()
 const PORT = process.env.PORT || 3311
+const DB_URL = process.env.DB_URL
+const DB_NAME = process.env.DB_NAME
 const apiV0router = require('src/api/v0')
 
 try {
