63e223a596b27a1ce2fed8b9ea3dacb148026fe7
laps78 <sovtem@gmail.com>
2024-11-20 19:51:05 +0300

    Обновлены стили дней календаря: вынесены в отдельный файл в папке компонента. В UI выделен дни текущего месяца border: grey

--- client-term/src/components/Calendar/CalendarUI.tsx
+++ client-term/src/components/Calendar/CalendarUI.tsx
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
+import "./calendar-style.css";
 
 export function CalendarUI() {
   const [calendarValue, setCalendarValue] = useState(new Date());
-
+  const dayClickHandler = (value, event) => {
+    console.log("calendarEvent: ", event.target);
+  };
   return (
     <div className="calendar__wrapper">
       <Calendar
         className="Calendar"
         onChange={setCalendarValue}
+        onClickDay={dayClickHandler}
         value={calendarValue}
-        showNavigation={false}
+        showNavigation={true}
       />
     </div>
   );
