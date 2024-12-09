268693f72920578335d250b7e01d1d967d4e09ee
laps78 <sovtem@gmail.com>
2024-12-09 07:47:46 +0300

    проведен рефакторинг корневого роутера v0 сервера t-checke4

--- src/api/v0/api_v0.js
+++ src/api/v0/api_v0.js
@@ -1,19 +1,25 @@
 const express = require('express')
 const api_router_v0 = express.Router()
 const saveDataToFS = require('../modules/saveToFS/saveToFS.js')
+const saveToDB = () => console.info('implement DB connector to represent this feature');
 
-const incomeHandler = (req, res, next) => {
+const dataSaver = (data) => {
+  saveDataToFS(data)
+  saveToDB(data)
+}
+
+const checkinHandler = (req, res, next) => {
   const { data } = req.data
   const dataString = data.toString()
-  saveDataToFS(dataString)
+  dataSaver(dataString)
   req.status(200)
   res.json('200 | Данные успешно сохранены')
 }
 
-const outcomeHandler = (req, res, next) => {
+const checkoutHandler = (req, res, next) => {
   const { data } = req.data
   const dataString = data.toString()
-  saveDataToFS(dataString)
+  dataSaver(dataString)
   res.status(200)
   res.json('200 | Данные успешно сохранены')
 }
@@ -23,8 +29,8 @@ const reportHandler = (req, res, next) => {
   res.json("Данный роут предусмотрен, но пока не реализован.")
 } 
 
-api_router_v0.post('income', incomeHandler)
-api_router_v0.post('outcome', outcomeHandler)
+api_router_v0.post('income', checkinHandler)
+api_router_v0.post('outcome', checkoutHandler)
 api_router_v0.get('report', reportHandler)
 
 module.exports = api_router_v0
