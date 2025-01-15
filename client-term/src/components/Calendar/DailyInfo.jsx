ecba36b6b35f0b01fdd05be33bb8f8e7f3f02711
laps78 <sovtem@gmail.com>
2025-01-15 17:04:00 +0300

    milestone: изменена cхема поучения данных. Требуетcя перепиcать statsMaker дlя slучая поlучения путого []. Poka eto vyzivaet oshibku: some changes added to index.

---
+++ client-term/src/components/Calendar/DailyInfo.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import EditMarkForm from "./editMark.form";
+
+const DailyInfo = ({ dayStats }) => {
+  console.log("daystats: ", dayStats);
+  const [isFormHidden, setFormHidden] = useState(true);
+  // open editors handlers
+  const editCheckinHandler = (event) => {
+    setFormHidden(!isFormHidden);
+    //setNowEditing(dailyMarks[0]);
+    //
+    console.log("isFormHidden: ", isFormHidden);
+    //
+  };
+  const editCheckoutHandler = (event) => {
+    setFormHidden(!isFormHidden);
+    //setNowEditing(dailyMarks[1]);
+    console.log("isFormHidden: ", isFormHidden);
+  };
+  // if (typeof dayStats === String) {
+  //   dayStats = {
+  //     checkinTimeString: false,
+  //     checkoutTimeString: false,
+  //     workedOutHours: false,
+  //     restMinutes: false,
+  //   };
+
+  return (
+    <>
+      <div className="daily_card_stats">
+        <div className="statsRow__container">
+          <span className="statsRow">
+            <strong>ПРИХОД:</strong>
+            {`${dayStats.checkinTimeString || "нет отметок"}`}
+          </span>
+          <a
+            className="statsRow_edit_link"
+            href="#"
+            onClick={editCheckinHandler}
+          >
+            ✎
+          </a>
+        </div>
+        <div className="statsRow__container">
+          <span className="statsRow">
+            <strong>УХОД:</strong>{" "}
+            {`${dayStats.checkoutTimeString || "нет отметок"}`}
+          </span>
+          <a
+            className="statsRow_edit_link"
+            href="#"
+            onClick={editCheckoutHandler}
+          >
+            ✎
+          </a>
+        </div>
+        <hr />
+        <span className="statsRow">
+          <strong>ОТРАБОТАНО:</strong> {dayStats.workedOutHours || 0} часов{" "}
+          {dayStats.restMinutes || 0} минут
+        </span>
+      </div>
+    </>
+  );
+};
+
+export default DailyInfo;
